0094c51a4600b1d5de7e423427cb9c1f141effb1
Abhi <s3666399@student.rmit.edu.au>
2018-07-30 08:53:33 +1000

    Occurances Field Added and Methods Changed
    Occurance field added which stores the number of occurances of each element and modified all the functions to support that.

--- Assignment1_ADA/src/LinkedListMultiset.java
+++ Assignment1_ADA/src/LinkedListMultiset.java
@@ -12,31 +12,49 @@ public class LinkedListMultiset<T> extends Multiset<T> {
 	public void add(T item) {
 		// Implement me!
 		Node new_node = new Node(item);
+		int occur = getOccurances(item);
+		new_node.setOccur(occur + 1);
 		if (head != null)
 			new_node.setNext(head);
-			head = new_node;
+		head = new_node;
 	} // end of add()
+
+	private int getOccurances(T item) {
+		if (head == null)
+			return 0;
+		Node start = head;
+		int occ = 0;
+		while (start != null) {
+			if (start.getValue().equals(item)) {
+				occ = start.getOccur();
+				start.setOccur(occ + 1);
+
+			}
+			start = start.getNext();
+		}
+		return occ;
+	}
 
 	public int search(T item) {
 		// Implement me!
 		if (head == null)
 			return 0;
 		Node start = head;
-		int count = 0;
-		while (start!= null) {
+		while (start != null) {
 			if (start.getValue().equals(item)) {
-				count++;
+				return start.getOccur();
 			}
 			start = start.getNext();
 		}
-		return count;
+		return 0;
 	} // end of add()
 
 	public void removeOne(T item) {
 		if (head == null)
 			return;
-		if(head.getValue().equals(item)) {
-			head=head.getNext();
+		if (head.getValue().equals(item)) {
+			head = head.getNext();
+			decreaseOccurances(item);
 			return;
 		}
 		Node start = head;
@@ -45,28 +63,47 @@ public class LinkedListMultiset<T> extends Multiset<T> {
 		while (start != null) {
 			if (start.getValue().equals(item)) {
 				prev.setNext(start.getNext());
+				decreaseOccurances(item);
 				return;
-			}
-			else
+			} else
 				prev = start;
 			start = start.getNext();
 		}
 
 	} // end of removeOne()
 
-	public void removeAll(T item) {
-		// Implement me!
+	private void decreaseOccurances(T item) {
+
 		if (head == null)
 			return;
-		while(head.getValue().equals(item)) {
-			head=head.getNext();
+		Node start = head;
+		while (start != null) {
+			if (start.getValue().equals(item)) {
+				start.setOccur(start.getOccur() - 1);
+			}
+			start = start.getNext();
+		}
+
+	}
+
+	public void removeAll(T item) {
+		// Implement me!
+		
+		if (head == null)
+			return;
+		int occur=getOccurances(item);
+		int count=0;
+		while (head!=null&&head.getValue().equals(item)&&occur>0) {
+			head = head.getNext();
+			occur--;
 		}
 		Node start = head;
 		Node prev = head;
-		int count = 0;
-		while (start != null) {
-			if (start.getValue().equals(item))
+		while (start != null&&occur>0) {
+			if (start.getValue().equals(item)) {
 				prev.setNext(start.getNext());
+				occur--;
+			}
 			else
 				prev = start;
 			start = start.getNext();
@@ -75,25 +112,18 @@ public class LinkedListMultiset<T> extends Multiset<T> {
 
 	public void print(PrintStream out) {
 		// Implement me!
-		HashMap hs=new HashMap();
-		Node start=head;
-		if(start==null)
+		HashSet hs = new HashSet();
+		Node start = head;
+		if (start == null)
 			return;
-		while(start!=null)
-		{
-			if(hs.containsKey(start.getValue()))
-			{
-				int count=(int)hs.get(start.getValue())+1;
-				hs.put(start.getValue(),count);
+		while (start != null) {
+			if (!hs.contains(start.getValue())) {
+				System.out.println(start.getValue() + " | " + start.getOccur());
+				hs.add(start.getValue());
 			}
-			else
-			{
-				hs.put(start.getValue(),1);
+			start = start.getNext();
 
-			}
-			start=start.getNext();
 		}
-		System.out.println(hs);
 	} // end of print()
 
 	/**
@@ -104,6 +134,16 @@ public class LinkedListMultiset<T> extends Multiset<T> {
 		protected T value;
 		/** Reference to next node. */
 		protected Node next;
+		/** Stored value of number of occur. */
+		protected int occur;
+
+		public int getOccur() {
+			return occur;
+		}
+
+		public void setOccur(int occur) {
+			this.occur = occur;
+		}
 
 		public Node(T value) {
 			this.value = value;
--- Assignment1_ADA/src/SortedLinkedListMultiset.java
+++ Assignment1_ADA/src/SortedLinkedListMultiset.java
@@ -1,34 +1,31 @@
 import java.io.PrintStream;
 import java.util.*;
 
-
-
-
 public class SortedLinkedListMultiset<T extends Comparable<T>> extends Multiset<T>  
 {
 	protected Node head;
-	
+
 	public SortedLinkedListMultiset() {
 		head=null;
 	} // end of SortedLinkedListMultiset()
-	
-	
+
+
 	public void add(T item) {
 		// Implement me!
-	//Node new_node=new Node(item);
+		//Node new_node=new Node(item);
 		Node new_node=new Node(item);
-		
-		
-	if(head==null) {
-		head=new_node;
-		return;
-	}
-	if(head.getValue().compareTo(item)>0) {
-		new_node.setNext(head);
-		head=new_node;
-	}
-	Node start=head;
-	Node prev=head;
+		new_node.setOccur(getOccurances(item)+1);
+
+		if(head==null) {
+			head=new_node;
+			return;
+		}
+		if(head.getValue().compareTo(item)>0) {
+			new_node.setNext(head);
+			head=new_node;
+		}
+		Node start=head;
+		Node prev=head;
 		while(start!=null) {
 			if(start.getValue().compareTo(item)>0)
 			{
@@ -43,29 +40,42 @@ public class SortedLinkedListMultiset<T extends Comparable<T>> extends Multiset<
 		if(prev!=null)
 			prev.setNext(new_node);
 		new_node.setNext(start);
-			
+
 	} // end of add()
-	
-	
-	public int search(T item) {
-		// Implement me!		
-	
+
+	private int getOccurances(T item) {
 		if (head == null)
 			return 0;
 		Node start = head;
-		int count = 0;
-		while (start!= null && start.getValue().compareTo(item)<=0) {
+		int occ = 0;
+		while (start != null) {
 			if (start.getValue().equals(item)) {
-				count++;
+				occ = start.getOccur();
+				start.setOccur(occ + 1);
+
 			}
-			
 			start = start.getNext();
 		}
-		return count;
-	
+		return occ;
+	}
+	public int search(T item) {
+		// Implement me!		
+
+		if (head == null)
+			return 0;
+		Node start = head;
+		while (start!= null && start.getValue().compareTo(item)<=0) {
+			if (start.getValue().equals(item)) {
+				return start.getOccur();
+			}
+
+			start = start.getNext();
+		}
+		return 0;
+
 	} // end of add()
-	
-	
+
+
 	public void removeOne(T item) {
 		// Implement me!
 
@@ -73,6 +83,7 @@ public class SortedLinkedListMultiset<T extends Comparable<T>> extends Multiset<
 			return;
 		if(head.getValue().equals(item)) {
 			head=head.getNext();
+			decreaseOccurances(item);
 			return;
 		}
 		Node start = head;
@@ -81,30 +92,46 @@ public class SortedLinkedListMultiset<T extends Comparable<T>> extends Multiset<
 		while (start != null) {
 			if (start.getValue().equals(item)) {
 				prev.setNext(start.getNext());
+				decreaseOccurances(item);
 				return;
 			}
 			else
 				prev = start;
 			start = start.getNext();
 		}
-
-	
 	} // end of removeOne()
-	
-	
+
+	private void decreaseOccurances(T item) {
+		if (head == null)
+			return;
+		Node start = head;
+		while (start != null) {
+			if (start.getValue().equals(item)) {
+				start.setOccur(start.getOccur() - 1);
+			}
+			start = start.getNext();
+		}
+
+	}
 	public void removeAll(T item) {
 		// Implement me!
 	} // end of removeAll()
-	
-	
+
+
 	public void print(PrintStream out) {
 		// Implement me!
-		Node start=head;
-		while(start!=null) {
-			System.out.print(start.getValue()+"->");
-		start=start.next;
+		HashSet hs = new HashSet();
+		Node start = head;
+		if (start == null)
+			return;
+		while (start != null) {
+			if (!hs.contains(start.getValue())) {
+				System.out.println(start.getValue() + " | " + start.getOccur());
+				hs.add(start.getValue());
+			}
+			start = start.getNext();
+
 		}
-		System.out.println();
 	} // end of print()
 	/**
 	 * Node type, inner private class.
@@ -114,7 +141,16 @@ public class SortedLinkedListMultiset<T extends Comparable<T>> extends Multiset<
 		protected T value;
 		/** Reference to next node. */
 		protected Node next;
+		/** Stored value of number of occur. */
+		protected int occur;
 
+		public int getOccur() {
+			return occur;
+		}
+
+		public void setOccur(int occur) {
+			this.occur = occur;
+		}
 		public Node(T item) {
 			// TODO Auto-generated constructor stub
 			value=item;
